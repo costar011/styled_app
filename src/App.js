965dda6ccd5d753360c606e11fd1a0d88ad1bbbb
costar011 <dnaer456@gmail.com>
2021-12-17 19:07:53 +0900

    index

--- src/App.js
+++ src/App.js
@@ -1,3 +1,30 @@
 import React from "react";
-import { Route } from "react-router-dom";
 import Button from "./Button";
+import Navigation from "./Layouts/Navigation";
+import Footer from "./Layouts/Footer";
+import { Route } from "react-router-dom";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      changeValue: 0,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Route path="/" component={Navigation} />
+
+        <h1>{this.state.changeValue}</h1>
+        <Button name={`UP`} />
+        <Button name={`DOWN`} />
+
+        <Route path="/" component={Footer} />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/Layouts/Navigation.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { NavLink, Route } from "react-router-dom";
+
+class Navigation extends React.Component {
+  render() {
+    return <div>NA</div>;
+  }
+}
+export default Navigation;
